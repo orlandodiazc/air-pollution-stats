c6dccccc4b8681319a2048fe9a5de98e6f917133
ditod1 <orlandodiazconde@gmail.com>
2023-03-21 12:23:06 -0500

    Refactor functions

--- netlify/functions/coordinates.cjs
+++ netlify/functions/coordinates.cjs
@@ -1,20 +1,19 @@
 const axios = require('axios')
 
 exports.handler = async function (event) {
-	const { lat, lon } = event.queryStringParameters
-	const API_KEY = 'f18c5efec7fe93374ccee2f15f9bc2a3'
-	const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+	const { city } = event.queryStringParameters
+	const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.API_KEY}`
 
 	try {
 		const { data } = await axios.get(url)
 		return {
-			status: 200,
+			statusCode: 200,
 			body: JSON.stringify(data),
 		}
 	} catch (error) {
 		const { status, statusText, headers, data } = error.response
 		return {
-			status,
+			statusCode: status,
 			body: JSON.stringify({ status, statusText, headers, data }),
 		}
 	}

15a6919104429ec9af19ec390c993a031991a798
Orlando Diaz <orlandodiazconde@gmail.com>
2023-05-26 07:01:24 -0500

    Fix lint

--- api/cityAirPollution.js
+++ api/cityAirPollution.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function (request, response) {
+export default async function cityAirPollution(request, response) {
   const { lat, lon } = request.query;
   const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
   const { data } = await axios.get(url);
--- api/coordinates.js
+++ api/coordinates.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default async function coordinates(request, response) {
   const { city } = request.query;
